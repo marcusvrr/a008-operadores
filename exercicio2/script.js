7a390a72f15a2016e9acf24e97199ec9fd1d0fae
marcusvrr <marcusvrr2@gmail.com>
2022-07-24 10:21:23 -0300

    revisão da thay

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,7 +1,9 @@
 let numA = Number(prompt('Digite um numero?'))
 let numB = Number(prompt('Digite outro numero?'))
 let resultado
+
 console.log(numA, numB)
+
 resultado = numA > numB
 console.log("O primeiro numero é maior que segundo?",resultado)
 resultado = numA === numB
